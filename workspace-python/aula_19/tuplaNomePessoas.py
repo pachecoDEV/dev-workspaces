1e4745da41093def8b12994fc9b253b30a796a56
APCOUTO <APCOUTO@APCOUTO-T480.br.oracle.com>
2020-08-11 22:19:53 -0300

    3 soluções diferentes para o exercício 6

--- workspace-python/aula_19/tuplaNomePessoas.py
+++ workspace-python/aula_19/tuplaNomePessoas.py
@@ -1,4 +1,6 @@
 # Solução 1
+print("**SOLUÇÃO 1**")
+
 nomes = ("Ana", "Bia", "Celi", "Diana", "Eva", "Fabia")
 combinacoes = []
 for i in nomes:
@@ -10,4 +12,27 @@ print(combinacoes)
 
 
 # Solução 2
+print("**SOLUÇÃO 2**")
+
 nomes = ("Ana", "Bia", "Celi", "Diana", "Eva", "Fabia")
+
+def dupla(nomes):
+    for x in nomes:
+        for y in nomes:
+            if x != y:
+                if nomes.index(y) > nomes.index(x):
+                    print(x + " e " + y)
+nome_teste = dupla(nomes)
+
+# Solução 3
+print("**SOLUÇÃO 3**")
+
+def combinacoes(nomes):
+    for a in range(len(nomes)):
+        for b in range(len(nomes)):
+            if (nomes[a] != nomes[b] and (b > a)):
+                print(nomes[a], "e", nomes[b])
+
+nomes = ("Ana", "Bia", "Celi", "Diana", "Eva", "Fabia")
+combinacoes(nomes)
+
