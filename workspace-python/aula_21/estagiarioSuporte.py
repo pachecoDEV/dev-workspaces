70df523703b9e67b190793f1a4779db3f2193b45
Alex Couto <pachecodocouto@gmail.com>
2020-08-26 14:45:00 -0300

    Exercicio Estagiário do Suporte

---
+++ workspace-python/aula_21/estagiarioSuporte.py
@@ -0,0 +1,4 @@
+
+numIdentificacao = int(input("Digite o número de identificação do ativo: "))
+tipoDefeito = input("Informe o tipo de defeito apresentado: ")
+menu = print("*** Lista de defeitos ***")
