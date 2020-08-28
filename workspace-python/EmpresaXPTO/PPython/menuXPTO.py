f064f6ad921438a3ed426b6e15a718b6c5296e1b
Alex Couto <pachecodocouto@gmail.com>
2020-08-28 18:12:17 -0300

    git updates.

--- workspace-python/EmpresaXPTO/PPython/menuXPTO.py
+++ workspace-python/EmpresaXPTO/PPython/menuXPTO.py
@@ -1,39 +1,103 @@
-import cadastrarproduto
-import alterarproduto
-import removeproduto
-
 opcao = 0
 
 codproduto = []
 descproduto = []
 qtdeproduto = []
-produtos = (codproduto, descproduto, qtdeproduto)
+estoque = []
 
 while opcao != 7:
     print('\nMenu')
     opcao = int(input(
-        "\t1. Cadastrar Produto\n\t2. Alterar Produto\n\t3. Excluir Produto\n\t4. Listar Estoque de Peça\n\t5. Comprar Produto\n\t6. Vender Produto\n\t7. Sair\n\tDigite a opção desejada: "))
+        "\t1. Cadastrar Produto\n\t"
+        "2. Alterar Produto\n\t"
+        "3. Excluir Produto\n\t"
+        "4. Listar Estoque de Peça\n\t"
+        "5. Comprar Produto\n\t"
+        "6. Vender Produto\n\t"
+        "7. Sair\n\t"
+        "Digite a opção desejada: "))
+
     if opcao == 1:
-        cadastrarproduto.cadastrarprodutos()
+        def cadastrarprodutos():
+            codproduto = int(input("Código: "))
+            if codproduto in estoque:
+                print("Código já cadastrado")
+            else:
+                descproduto = input("Descrição do produto: ").upper()
+                qtdeproduto = int(input("Quantidade: "))
+                estoque.append(codproduto)
+                estoque.append(descproduto)
+                estoque.append(qtdeproduto)
+
+        cadastrarprodutos()
 
     elif opcao == 2:
-        senha = input("Digite sua Senha: ")
+        cont = 3
+        senha = input("Digite sua senha: ")
+        while senha != "yN1825*a" and cont > 1:
+            print("Senha incorreta, você têm mais ", cont - 1, " tentativas.")
+            senha = input("Digite novamente.\nSenha: ")
+            cont = cont - 1
         if senha == "yN1825*a":
             print("Acesso permitido.")
-            alterarproduto.alterarproduto()
+
+            def alterarproduto():
+                codproduto = input("Código: ")
+                index = codproduto.index(codproduto)
+                if codproduto in estoque:
+                    print("Descrição: ", descproduto[index],
+                          "Quantidade: ", qtdeproduto[index])
+                    descproduto[index] = input("Nova descrição: ").upper()
+                    qtdeproduto[index] = int(input("Nova quantidade: "))
+                    print("Descrição alterada para: ", descproduto[index],
+                          "Quantidade alterada para: ", qtdeproduto[index])
+                else:
+                    print("Produto não cadastrado.")
+
+            alterarproduto()
+
         else:
-            print("SENHA INCORRETA")
+            print("SENHA BLOQUEADA!")
 
     elif opcao == 3:
-        senha = input("Digite sua Senha: ")
+        cont = 3
+        senha = input("Digite sua senha: ")
+        while senha != "yN1825*a" and cont > 1:
+            print("Senha incorreta, você têm mais ", cont - 1, " tentativas.")
+            senha = input("Digite novamente.\nSenha: ")
+            cont = cont - 1
         if senha == "yN1825*a":
-            print("ACESSO PERMITIDO")
-            removeproduto.removerproduto()
+            print("Acesso permitido.")
+
+            def removerproduto():
+                codproduto = int(input("Código: "))
+                if codproduto in estoque:
+                    print("Descrição: ", descproduto, "Quantidade: ", qtdeproduto)
+                    opcao = input("Deseja excluir o produto? [S] ou [N]").upper()
+                    if opcao == "s".upper():
+                        codproduto.pop(codproduto.index(codproduto))
+                        descproduto.pop(codproduto.index(codproduto))
+                        qtdeproduto.pop(codproduto.index(codproduto))
+                        print("PRODUTO EXCLUIDO COM SUCESSO")
+                    elif opcao == "n".upper():
+                        print("PRODUTO NÃO EXCLUÍDO")
+                    else:
+                        print("OPÇÃO INVÁLIDA")
+                else:
+                    print("PRODUTO NÃO CADASTRADO")
+
+            removerproduto()
+
         else:
-            print("SENHA INCORRETA")
+            print("Senha Bloqueada!")
             
     elif opcao == 4:
-        print(sorted(produtos))
+        def listarprodutos():
+            print("CÓDIGO\tDESCRIÇÃO\tQUANTIDADE EM ESTOQUE:")
+            print(6 * '-', '', 9 * '-', ' ', 22 * '-')
+            print(estoque)
+
+        listarprodutos()
 
     elif opcao == 5:
         print("Comprar produto")
